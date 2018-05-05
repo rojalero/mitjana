100431c9e59080f2efa4e6d03df881e14e3b2b4e
Rojalero <rojalero@hotmail.com>
2018-05-05 17:53:53 +0200

    Segon commit del projecte mitjana

--- mitjana.c
+++ mitjana.c
@@ -14,7 +14,7 @@ int main(){
 		suma = suma + nou_valor;
 		numero_repeticions = numero_repeticions + 1;
 		printf("Vols continuar (s/n): ");
-		scanf(" %c", &consulta);
+		scanf("%c", &consulta);
 	}
 
 	mitjana = suma / numero_repeticions;
